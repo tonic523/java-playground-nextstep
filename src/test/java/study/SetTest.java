80178c7248b53005dab23b5747379e13bd2d0c64
wooseok <diddmlvkf@gmail.com>
2021-11-22 15:10:50 +0900

    refactor(SetTest) : contains() 메서드의 false 일 때 테스트케이스 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,10 +31,13 @@ public class SetTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3})
+    @CsvSource(
+            value = {"1:true", "2:true", "3:true", "4:false", "5:false"},
+            delimiter = ':'
+    )
     @DisplayName("contains()")
-    void contains(int number) {
-        assertThat(numbers.contains(number)).isTrue();
+    void contains(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 
 }
