8854ff540add5204a2e2c4762055cf0775e1bb30
wooseok <diddmlvkf@gmail.com>
2021-11-23 22:11:48 +0900

    feat(ApplicationTest) : 입력값 검증 단위 테스트 구현

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -2,7 +2,10 @@ package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 
@@ -17,4 +20,39 @@ public class ApplicationTest {
                 new int[] {1, 2, 3}
         ));
     }
+
+    @DisplayName("문자가 1~9인지 검증")
+    @Nested
+    class ValidateChar {
+        @DisplayName("성공")
+        @Test
+        void success() {
+            assertThat(application.validateChar('9')).isTrue();
+        }
+
+        @DisplayName("실패")
+        @Test
+        void fail() {
+            assertThat(application.validateChar('0')).isFalse();
+        }
+    }
+
+    @DisplayName("입력값 검증")
+    @Nested
+    class ValidateInput {
+        @DisplayName("성공")
+        @ParameterizedTest
+        @ValueSource(strings = {"123", "159"})
+        void success(String input) {
+            assertThat(application.validateInput(input)).isTrue();
+        }
+
+        @DisplayName("실패")
+        @ParameterizedTest
+        @ValueSource(strings = {"012", "1599", "abc"})
+        void fail(String input) {
+            assertThat(application.validateInput(input)).isFalse();
+        }
+    }
+
 }
