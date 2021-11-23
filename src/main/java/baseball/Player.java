78ef55d9f0a6a196295fda46d7971321d8dffac4
wooseok <diddmlvkf@gmail.com>
2021-11-23 16:17:39 +0900

    feat(Player) : 플레이어의 기능 목록 구현

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,46 @@
+package baseball;
+
+import java.util.*;
+
+public class Player {
+    private static final int SIZE = 3;
+    private List<Integer> numbers = new ArrayList<Integer>();
+
+    public Player(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public Player() {
+    }
+
+    public void setNumbers(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public Map<String, Integer> getHint(List<Integer> numbers) {
+        Map<String, Integer> hint = new HashMap<>();
+        hint.put("strike", 0);
+        hint.put("ball", 0);
+        hint.put("nothing", 0);
+        for (int i = 0; i < SIZE; i++) {
+            int number = numbers.get(i);
+            String type = getType(number, i);
+            hint.replace(type, hint.get(type) + 1);
+        }
+        return hint;
+    }
+
+    public String getType(int number, int index) {
+        if (numbers.contains(number)) {
+            return isBallStrike(number, index);
+        }
+        return "";
+    }
+
+    public String isBallStrike(int number, int index) {
+        if (index == numbers.indexOf(number)) {
+            return "strike";
+        }
+        return "ball";
+    }
+}
