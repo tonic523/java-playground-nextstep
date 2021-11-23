3258fea601c24288415fd6251c5eae04fb6f6d6d
wooseok <diddmlvkf@gmail.com>
2021-11-23 22:31:13 +0900

    feat(Application) : 숫자야구게임 실행 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,15 +1,41 @@
 package baseball;
 
+import java.util.Map;
 import java.util.Scanner;
 
 public class Application {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        Player computer = new Player();
+        int play = 1;
+        while (play == 1) {
+            Prompt.input();
+            String input = scanner.nextLine();
+            // 잘못된 입력값
+            if (!(validateInput(input))) {
+                Prompt.inputError();
+                continue;
+            }
+
+            int[] numbers = convertToNumbers(input);
+            Map<String, Integer> hint = computer.getHint(numbers);
+            if (hint.get("nothing") == 3) {
+                System.out.println("낫싱");
+                continue;
+            }
+            if (hint.get("strike") == 3) {
+                Prompt.success();
+                play = scanner.nextInt();
+                scanner.nextLine();
+                continue;
+            }
+            Prompt.hint(hint.get("ball"), hint.get("strike"));
+        }
 
     }
 
-    public int[] convertToNumbers(String str) {
+    public static int[] convertToNumbers(String str) {
         int[] numbers = new int[Player.SIZE];
         for (int i = 0; i < Player.SIZE; i++) {
             numbers[i] = str.charAt(i) - '0';
@@ -17,7 +43,7 @@ public class Application {
         return numbers;
     }
 
-    public boolean validateInput(String str) {
+    public static boolean validateInput(String str) {
         if (str.length() != Player.SIZE) {
             return false;
         }
@@ -29,7 +55,7 @@ public class Application {
         return true;
     }
 
-    public boolean validateChar(char c) {
+    public static boolean validateChar(char c) {
         if ('1' <= c && c <= '9') {
             return true;
         }
